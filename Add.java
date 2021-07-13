44b91f006ca4993ab7db8ac85aa44339bcb6e0b5
unknown <ansala4lulla@gmail.com>
2021-07-13 23:00:17 +0530

    adding java file

---
+++ Add.java
@@ -0,0 +1,15 @@
+
+public class Add {
+	
+	public static void main(String args[])
+	{
+		float num1 = 0.5f;
+		int num2 = 60;
+		float sum=num1+num2;
+		
+	
+		System.out.println("Sum of these numbers = "+sum);
+		
+	}
+;
+}
